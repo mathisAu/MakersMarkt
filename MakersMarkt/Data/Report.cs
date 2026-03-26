34e10008df51596c292a7810be9c45ebdf9a170d
martwijnman <153086626+martwijnman@users.noreply.github.com>
2026-03-26 13:59:58 +0100

    Add report

---
+++ MakersMarkt/Data/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakersMarkt.Data
+{
+    internal class Report
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string ReportDescription { get; set;  }
+    }
+}
